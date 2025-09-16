162c21d66f98235eb8ddc11c69f6c0c8043e1ba5
ovavova <vyereschenko@gmail.com>
2025-09-16 20:08:58 +0300

    testing git commit

--- prelim_tests/main.py
+++ prelim_tests/main.py
@@ -1,2 +1,7 @@
-print('Hello world!!!!!!!')
-print('Hello GIT')
+import numpy as np
+
+a = np.matrix("1,2,5;1,-1,3;3,-6,-1")
+b = np.matrix("-9;2;25")
+a_inv = np.linalg.inv(a)
+x = a_inv.dot(b)
+print(x)
