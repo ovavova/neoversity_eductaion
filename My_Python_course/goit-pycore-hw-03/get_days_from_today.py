622ca0a5de400ed25b1a680516fb91915794f985
ovavova <vyereschenko@gmail.com>
2025-10-08 12:55:15 +0300

    added try catching value error

--- My_Python_course/goit-pycore-hw-03/get_days_from_today.py
+++ My_Python_course/goit-pycore-hw-03/get_days_from_today.py
@@ -3,13 +3,16 @@
 from datetime import datetime, date
 
 #date = input("Input the date, format 'YYYY-MM-DD'': ")
-inp_date = "2025-10-11"
+inp_date = "2021-09-18"
 
 def get_days_from_today(date: str) -> int: 
-    inpt_date = datetime.strptime(inp_date,"%Y-%m-%d")       # converting from string to datetime
+    try:
+        inpt_date = datetime.strptime(inp_date,"%Y-%m-%d")       # converting from string to datetime
+    except ValueError:
+        return 'Input format doesnt match. Try again: "YYYY-MM-DD"'
     today = datetime.now()                                   # todays current date 
     diff = today - inpt_date                                 # days from user date to current date
     int_diff = int(diff.days)                                # converting from timedeltaobject to int
     return int_diff
 
-print(f"There are {get_days_from_today(date)} days from the date to todays date {date.today()}")
+print(f"{get_days_from_today(date)}")
