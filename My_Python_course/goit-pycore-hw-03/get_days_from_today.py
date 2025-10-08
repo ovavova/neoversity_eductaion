95f52988ccd69bd7f63a0057d700df2a95ca5a42
ovavova <vyereschenko@gmail.com>
2025-10-08 15:21:27 +0300

    hw 2

--- My_Python_course/goit-pycore-hw-03/get_days_from_today.py
+++ My_Python_course/goit-pycore-hw-03/get_days_from_today.py
@@ -3,11 +3,11 @@
 from datetime import datetime, date
 
 #date = input("Input the date, format 'YYYY-MM-DD'': ")
-inp_date = "2021-09-18"
+inp_date = "2030-09-18"
 
 def get_days_from_today(date: str) -> int: 
     try:
-        inpt_date = datetime.strptime(inp_date,"%Y-%m-%d")       # converting from string to datetime
+        inpt_date = datetime.strptime(inp_date,"%Y-%m-%d")   # converting from string to datetime
     except ValueError:
         return 'Input format doesnt match. Try again: "YYYY-MM-DD"'
     today = datetime.now()                                   # todays current date 
--- My_Python_course/goit-pycore-hw-03/get_numbers_ticket.py
+++ My_Python_course/goit-pycore-hw-03/get_numbers_ticket.py
@@ -3,3 +3,22 @@
 
 Вам необхідно написати функцію get_numbers_ticket(min, max, quantity), яка допоможе генерувати набір унікальних випадкових чисел
 для таких лотерей. Вона буде повертати випадковий набір чисел у межах заданих параметрів, причому всі випадкові числа в наборі повинні бути унікальні """
+
+import random
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:                # min >= 1; max <=1000; quantity 
+    if min < 1 or max > 1000 or max < min or quantity < min or quantity > max :   # parameters check
+        return "Please check parameters. min >= 1; max <=1000; quantity between min and max"  
+    rndm_list = []
+    i = 0
+    while i < quantity:
+        rnd_num = random.randint(min, max) # generating random number
+        if rnd_num not in rndm_list:       # check if number is unique 
+            rndm_list.append(rnd_num)      # adding to list if unique 
+            i +=1                          # iterating
+    rndm_list.sort()                       # sorting list
+    return rndm_list
+
+print(get_numbers_ticket(1,100,10))
+
+    
